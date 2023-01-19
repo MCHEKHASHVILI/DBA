4708482a970d516cc52b772789f4e4a595896051
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-01-19 18:28:46 +0400

    corrected

---
+++ main.js
@@ -0,0 +1,14 @@
+const toDoList = [
+    {
+        id: 1,
+        title: "my todo item"
+    },
+    {
+        id: 2,
+        title: "second todo item"
+    },
+]
+
+/**
+ * Create Todo List Functionality
+ */
